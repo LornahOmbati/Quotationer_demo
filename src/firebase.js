459ef92cfef401d5fe4fd1fa4d9e6f290553250f
LornaOmbati <lornah.ombati@strathmore.edu>
2024-10-02 10:37:41 +0300

    firebasechanges

--- src/firebase.js
+++ src/firebase.js
@@ -1,20 +1,21 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 import { getAnalytics } from "firebase/analytics";
+import { getAuth, GoogleAuthProvider } from 'firebase/auth';
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyD1HnPaaFBo4GOoc54LDxifZyhoaSZSkD0",
-  authDomain: "quotation-generator-2aa1e.firebaseapp.com",
-  projectId: "quotation-generator-2aa1e",
-  storageBucket: "quotation-generator-2aa1e.appspot.com",
-  messagingSenderId: "143620852986",
-  appId: "1:143620852986:web:d0a7d20a0cad57e1628d43",
-  measurementId: "G-LNPGFVCWWF"
+  apiKey: "AIzaSyCnTOezmZn1DJotp-bxoiZkfvJglwqV7Iw",
+  authDomain: "quotationerdemo.firebaseapp.com",
+  projectId: "quotationerdemo",
+  storageBucket: "quotationerdemo.appspot.com",
+  messagingSenderId: "585467668274",
+  appId: "1:585467668274:web:cd3da6c8fc2585202b992b",
+  measurementId: "G-B0ZCEVNTEW"
 };
 
 // Initialize Firebase
